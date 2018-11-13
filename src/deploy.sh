1758cb735b67f3502ae06b872ed5464660b5f92d
thecodearcher <brian.iyoha@gmail.com>
2018-11-14 00:42:44 +0100

    update deploy script

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,33 +1,52 @@
 #!/bin/sh
 
-# activate maintenance mode
-# php artisan down
 
 # get private key and add too ssh client
 echo $PRIVATE_KEY > private_key_file.txt
 cat private_key_file.txt | base64 --decode > private_key
 chmod 600 private_key
 eval `ssh-agent -s`
-ssh-agent $(ssh-add private_key; git pull origin master)
+ssh-agent $(ssh-add private_key);
+#ssh to server and run commands
+ssh root@social.timbu.com <<EOF
+    git pull origin master
+    cd src
 
-#git pull
+    # activate maintenance mode
+    php artisan down
 
-# ssh root@social.timbu.com -v
-ls
-
-# update PHP dependencies
-export COMPOSER_HOME='/tmp/composer'
-
-composer install --no-interaction
+    composer install --no-interaction --no-dev
 	# --no-interaction	Do not ask any interactive question
 	# --no-dev		Disables installation of require-dev packages.
-	# --prefer-dist		Forces installation from package dist even for dev versions.
 
-# update database
-php artisan migrate --force
+    # Clear caches
+    php artisan cache:clear
+
+
+    # Clear and cache routes
+    php artisan route:clear
+    php artisan route:cache
+
+    # Clear and cache config
+    php artisan config:clear
+    php artisan config:cache
+
+    # Install node modules
+    npm install
+
+    # Build assets using Laravel Mix
+    npm run production
+
+    # update database
+    php artisan migrate --force
 	# --force		Required to run when in production.
-# seed database
-php artisan db:seed --force
 
-# stop maintenance mode
-php artisan up
+    # seed database
+    php artisan db:seed --force
+
+    # stop maintenance mode
+    php artisan up
+EOF
+
+# update PHP dependencies
+
