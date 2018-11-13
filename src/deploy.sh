84df8253805440d92f3db204e219680cda8163fa
thecodearcher <brian.iyoha@gmail.com>
2018-11-13 14:02:10 +0100

    trying deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
+# activate maintenance mode
+php artisan down
+
+# update source code
+#git pull
+eval `ssh-agent -s`
+ssh-agent $(ssh-add ~/.ssh/id_rsa; git pull origin master)
 ls
-# # activate maintenance mode
-# php artisan down
 
-# # update source code
-# #git pull
-# eval `ssh-agent -s`
-# ssh-agent $(ssh-add ~/.ssh/id_rsa; git pull origin master)
+# update PHP dependencies
+export COMPOSER_HOME='/tmp/composer'
 
-# # update PHP dependencies
-# export COMPOSER_HOME='/tmp/composer'
+composer install --no-interaction
+	# --no-interaction	Do not ask any interactive question
+	# --no-dev		Disables installation of require-dev packages.
+	# --prefer-dist		Forces installation from package dist even for dev versions.
 
-# composer install --no-interaction
-# 	# --no-interaction	Do not ask any interactive question
-# 	# --no-dev		Disables installation of require-dev packages.
-# 	# --prefer-dist		Forces installation from package dist even for dev versions.
+# update database
+php artisan migrate --force
+	# --force		Required to run when in production.
+# seed database
+php artisan db:seed --force
 
-# # update database
-# php artisan migrate --force
-# 	# --force		Required to run when in production.
-# # seed database
-# php artisan db:seed --force
-
-# # stop maintenance mode
-# php artisan up
+# stop maintenance mode
+php artisan up
